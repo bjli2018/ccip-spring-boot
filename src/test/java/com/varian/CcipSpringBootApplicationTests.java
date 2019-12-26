b901129ba412b713f60378b4530f25fc37ce996f
dkq6686 <Bob.Li@varian.com>
2019-12-26 17:56:59 +0800

    2019-12-26 17:56 提交

---
+++ src/test/java/com/varian/CcipSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcipSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
